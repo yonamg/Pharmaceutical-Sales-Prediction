959642b58192eac2a40441a884b71167cc364d03
yonamg <softeng2006@gmail.com>
2022-09-09 23:13:52 +0300

    dashboard developing

---
+++ dashboard/app.py
@@ -0,0 +1,19 @@
+import user_overview_page
+import promotion_page
+# import user_experience_page
+# import user_satisifaction_page 
+
+import streamlit as st
+
+PAGES = {
+     "Data Overview": user_overview_page,
+     "Promotion Effect on Stores": promotion_page
+
+#     "User Engagement Analysis":  user_engagement_page,
+#     "User Experience Analytics": user_experience_page,
+#     "User Satisfaction Analysis": user_satisifaction_page
+}
+
+selection = st.sidebar.radio("Go to page", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
